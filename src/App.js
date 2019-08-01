22f2bc84149305b3681c6d318243f86edc881122
dominicgreen <dominic.green@karmoochi.com>
2019-08-01 16:27:58 +1200

    Landing page

--- src/App.js
+++ src/App.js
@@ -11,9 +11,59 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          FrushFood
+          Frush Food powered by Karmoochi
         </header>
       </div>
+
+<div data-role="page" id="order">
+  <div data-role="header" data-theme="b">
+    <a href="#profile" data-transition="flip" data-icon="gear" data-iconpos="right">Profile</a>
+    <h1>My Order</h1>
+  </div>
+
+<div data-role="content">
+
+<form id='orderForm' method='post' action='confirmation.php'>
+  <fieldset data-role="controlgroup">
+<button class="ui-icon" disabled="true" data-theme="b">Order date</button>
+<select type="range" name="order-date" id="order-date"></select>
+  </fieldset>
+  
+
+
+
+  <button type="submit" id='Purchase' value="Order" data-theme="b">Order</button>
+</form>
+
+</div>
+
+<div data-role="footer" data-theme="b">
+<h4>Frush Food</h4>
+</div>
+
+</div>
+
+
+
+<div data-role="page" id="profile">
+
+<div data-role="header"  data-theme="b">
+  <a href="#order" data-transition="flip" data-icon="delete" data-iconpos="right">Cancel</a>
+  <h1>Profile</h1>
+  <a href="#order" data-transition="flip" data-icon="check" data-iconpos="right">Save</a>
+</div>
+
+<div data-role="content">
+  <p>Profile page to confirm or update your account details</p>
+
+  
+
+</div>
+
+<div data-role="footer"  data-theme="b">
+  <h4>Frush Food</h4>
+</div>
+</div>
     );
   }
 }
